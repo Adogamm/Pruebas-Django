5d0e0e8e2a1f299d4c0c2aa1368960cbefd88925
Adolfo León <1718110396@utectulancingo.edu.mx>
2019-12-31 11:13:05 -0600

    Cambios en el administrador

--- libro/libro/applications/biblioteca/admin.py
+++ libro/libro/applications/biblioteca/admin.py
@@ -3,5 +3,31 @@ from django.contrib import admin
 # Register your models here.
 from .models import Autor, Libros
 
-admin.site.register(Autor)
-admin.site.register(Libros)
+#Clase para el administrador del autor
+class AutorAdmin(admin.ModelAdmin):
+    list_display = (
+        'nombre',
+        'nacionalidad',
+        'id'
+    )
+    #Atributo para buscar dentro de un campo
+    search_fields = ('nombre', 'nacionalidad',)
+
+
+#Clase para el administrador del libro
+class LibrosAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'resumen',
+        'autor',
+        'id'
+    )
+    #Atributo para buscar dentro de un campo
+    search_fields = ('title',)
+    #Atributo para filtro de autor
+    list_filter = ('autor',)
+
+
+
+admin.site.register(Autor, AutorAdmin)
+admin.site.register(Libros, LibrosAdmin)
