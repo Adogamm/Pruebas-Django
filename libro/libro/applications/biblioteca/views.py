323fd5fc8f72103d23d0f9c219810112645ead85
Adolfo León <1718110396@utectulancingo.edu.mx>
2020-01-02 18:06:10 -0600

    Inserciones desde los templates, filtros en los templates etc

--- libro/libro/applications/biblioteca/views.py
+++ libro/libro/applications/biblioteca/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from .models import Autor, Libros
+from django.views.generic import (
+    TemplateView,
+    ListView,
+    CreateView,
+)
 
-# Create your views here.
+class ListaAutores(ListView):
+    template_name = "biblioteca/lista-autores.html"
+    model = Autor
+    context_object_name = "autores"
+
+#Vista para listar libros de un autor
+class ListaLibrosAutores(ListView):
+    template_name = "biblioteca/lista-libros.html"
+    context_object_name = "libros"
+
+    def get_queryset(self):
+        #Identificador de autor
+        id = self.kwargs['pk']
+
+        #Filtro de los libros
+        lista = Libros.objects.filter(
+            autor = id
+        )
+        #Mostrar resultado
+        return lista
+
+    #Vista para registrar un nuevo autor
+class AddAutor(CreateView):
+    template_name = 'biblioteca/autor-add.html'
+    model = Autor
+    fields = ['nombre', 'nacionalidad']
+    success_url = "/"
